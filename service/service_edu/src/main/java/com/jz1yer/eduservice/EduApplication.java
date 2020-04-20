508bdf4db56f48437d21ad119989e9c239e9ce13
jz1yer <2920123@qq.com>
2020-04-21 04:49:55 +0800

    公共模块

--- service/service_edu/src/main/java/com/jz1yer/eduservice/EduApplication.java
+++ service/service_edu/src/main/java/com/jz1yer/eduservice/EduApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan(basePackages ="com.jz1yer" )
+@ComponentScan(basePackages ="com.jz1yer" )
 public class EduApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduApplication.class);
